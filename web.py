361825e6854c7e4e7fa1992cf63d7d1f58599f7e
Aaron Ganiz <83104453+BaldKlyntar@users.noreply.github.com>
2026-02-18 18:36:19 +0000

    Correccion_Sitio_Web_Dinamico

--- web.py
+++ web.py
@@ -1,53 +1,31 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
+from sitio import html as content
 
 
 class WebRequestHandler(BaseHTTPRequestHandler):
     def url(self):
         return urlparse(self.path)
 
+    def ruta(self):
+        return self.url().path
+
     def query_data(self):
         return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
 
-        # Se crea el diccionario contenido, cada key representa una ruta
-        # y el valor almacena el nombre del archivo html que se va a renderizar
-        contenido = {
-            "/": "home.html",
-            "/proyecto/1": "1.html",
-            "/proyecto/2": "2.html",
-            "/proyecto/3": "3.html",
-        }
-
-        # en la variable file se almacenan los nombres de los archivos html
-
-        file = contenido.get(self.path)
-        # Si el archivo se encuentra se abre y se lee con rb
-        # posteriormentese envia el status code, los headers y se escribe para presentarlo
-        # en pantalla
-        if file:
-            
-            # Se abre el archivo utilizando open
-            #el archivo es leido en su formato binario (rb)
-            with open(file, "rb") as file:
-                content = file.read()
-
-
+        ruta = self.ruta()
+        if ruta in content:
+            html = content[ruta]
             self.send_response(200)
             self.send_header("Content-Type", "text/html")
             self.end_headers()
-            self.wfile.write(content)
-            #self.wfile.write(self.get_response().encode("utf-8"))
+            self.wfile.write(html.encode(encoding="utf-8"))
 
-        # En el caso de no encontrar la ruta se realiza el mismo procedimiento
-        # enviando un status code 404 not found y el archivo de error.
         else:
-            self.send_response(404)
-            self.send_header("Content-Type", "text/html")
-            self.end_headers()
-            with open("notFound.html", "rb") as file:
-                self.wfile.write(file.read())
+            self.send_error(404)
+
 
     #En la funcion get_response, accedo al diccionario creado por parse_qsl a partir
     # de la ruta url y accedo a la key autor para extraer el nombre
